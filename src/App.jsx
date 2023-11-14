cbaad646e99fc1ac4bf2a1cb8f0b971cd186d0b3
Roman <korzunroman56@gmail.com>
2023-11-14 17:16:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="my-10  text-yellow-800  text-9xl text-center">fff</div>
+      <div className="my-10  text-yellow-800  text-9xl text-center">fff</div>
+      <div className="my-10  text-yellow-800  text-9xl text-center">fff</div>
+      <div className="my-10  text-yellow-800  text-9xl text-center">fff</div>
+      <div className="my-10  text-yellow-800  text-9xl text-center">fff</div>
+      <div className="my-10  text-yellow-800  text-9xl text-center">fff</div>
+      <div className="my-10  text-yellow-800  text-9xl text-center">fff</div>
+    </>
+  );
+}
+
+export default App;
