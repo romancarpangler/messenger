1b51949edbbcba2ddf52142735d2185b08b0f8a6
Roman <korzunroman56@gmail.com>
2023-11-18 12:48:50 +0200

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 function App() {
   return (
     <>
-      <div className="my-10  text-yellow-800  text-9xl text-center">fff</div>
-      <div className="my-10  text-yellow-800  text-9xl text-center">fff</div>
-      <div className="my-10  text-yellow-800  text-9xl text-center">fff</div>
-      <div className="my-10  text-yellow-800  text-9xl text-center">fff</div>
-      <div className="my-10  text-yellow-800  text-9xl text-center">fff</div>
-      <div className="my-10  text-yellow-800  text-9xl text-center">fff</div>
-      <div className="my-10  text-yellow-800  text-9xl text-center">fff</div>
+      <div className="my-10  text-yellow-800  text-9xl text-center">
+        abaga galamaga
+      </div>
     </>
   );
 }
